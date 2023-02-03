f5e2c071fcdea5aa5b5ae7254446cb0ffe51d8cf
cocacola-lover <pegas620570@gmail.com>
2023-02-03 20:03:44 +0300

    Rework boards around mappings and add new board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,43 +1,62 @@
 import { useState, useEffect, useRef } from "react";
 
 import { SensibleTile } from "./Tile";
-import { MovableChessPiece, KnightSVG, FlagSVG } from "./ChessPiece";
+import { ChessPiece, MovableChessPiece, KnightSVG, FlagSVG } from "./ChessPiece";
 import Position from "../logic/position";
 
 import './css/Board.css';
 
 const size = 600;
 
-interface Square {
-    distanceFromStart : number | null,
-    shortestPath : Square | null,
-    isPassable : boolean,
-    other : Object | null
-}
-
 interface BaseBoardProps {
-    height : number,
-    width : number,
-    squares : Square[][],
+    height : number
+    width : number
+    getPassability : (position : Position) => boolean
     knightPosition : Position
     flagPosition : Position
     boardRef? : React.RefObject<HTMLDivElement>
-    createTile : (square : Square, pos: Position) => JSX.Element
+    createTile : (passable : boolean, pos: Position) => JSX.Element
 
 }
 
 // Base function
 
 function BaseBoard (props : BaseBoardProps) {
+
+    const createBoard = () => {
+        const ans = [];
+        for (let i = 0; i < props.height; i++){
+            ans.push([]);
+            for (let j = 0; j < props.width; j++) {
+                const position = new Position(j, i);
+                ans.push(
+                    props.createTile(props.getPassability(position), position)
+                );
+            }
+        }
+        return ans;
+    }
+
+    // Weird drag bug prevention
+    useEffect(() => {
+        if (props.boardRef === undefined) return;
+        const board = props.boardRef.current;
+
+        const preventDragOrAnimation = (e : Event) => e.preventDefault();
+        
+        board?.addEventListener("dragstart", preventDragOrAnimation);
+        board?.addEventListener("dragover", preventDragOrAnimation);
+        return () => {
+            board?.removeEventListener("dragstart", preventDragOrAnimation);
+            board?.removeEventListener("dragover", preventDragOrAnimation);
+        }
+    })
+
     return (
         <div className="Board" 
              ref={props.boardRef} 
              style={{gridTemplate : `repeat(${props.height}, 1fr) / repeat(${props.width}, 1fr)`, width : `${size}px`, height : `${size * props.height / props.width}px`}}>
-                {
-                props.squares.map((row, index1) => 
-                        row.map((square, index2) => 
-                            props.createTile(square, new Position(index1, index2))))
-                }
+                {createBoard()}
         </div>
         )}
 
@@ -46,7 +65,7 @@ function BaseBoard (props : BaseBoardProps) {
 interface MovableBoardProps {
     height : number,
     width : number,
-    squares : Square[][],
+    getPassability : (position : Position) => boolean
     knightPosition : Position
     flagPosition : Position
     setKnightPosition : React.Dispatch<React.SetStateAction<Position>>
@@ -64,20 +83,6 @@ export function MovableBoard (props : MovableBoardProps) {
 
     
     const boardRef = useRef<HTMLDivElement>(null);
-
-    // Weird drag bug prevention
-    useEffect(() => {
-        const board = boardRef.current;
-
-        const preventDragOrAnimation = (e : Event) => e.preventDefault();
-        
-        board?.addEventListener("dragstart", preventDragOrAnimation);
-        board?.addEventListener("dragover", preventDragOrAnimation);
-        return () => {
-            board?.removeEventListener("dragstart", preventDragOrAnimation);
-            board?.removeEventListener("dragover", preventDragOrAnimation);
-        }
-    })
 
     // Watching for DragBalance
     useEffect( () => {
@@ -126,13 +131,14 @@ export function MovableBoard (props : MovableBoardProps) {
     }
 
     return <BaseBoard boardRef={boardRef} 
-            height={props.height} width={props.width} squares={props.squares} knightPosition={props.knightPosition} flagPosition={props.flagPosition}
+            height={props.height} width={props.width} getPassability={props.getPassability} knightPosition={props.knightPosition} flagPosition={props.flagPosition}
             createTile={
-                (square : Square, pos : Position) => 
+                (passable, pos) => 
                 <SensibleTile 
                     key={`${pos.y},${pos.x}`} 
                     onDragEnter={() => {setShadow(pos)}}
-                    black={(pos.y + pos.x) % 2 === 1}>
+                    black={(pos.y + pos.x) % 2 === 1}
+                    passable={passable}>
 
                     {/* Put knight and flag to their place */}
 
@@ -147,5 +153,84 @@ export function MovableBoard (props : MovableBoardProps) {
                     }
 
                 </SensibleTile>                
+            }/>
+}
+
+interface DrawableBoardProps {
+    height : number
+    width : number
+    knightPosition : Position
+    flagPosition : Position
+    getPassability : (position : Position) => boolean
+    setPassability : (position : Position, value : boolean) => void
+}
+
+enum PenState {
+    Inactive,
+    DrawPassable,
+    DrawImpassable
+}
+
+export function DrawableBoard (this: any, props : DrawableBoardProps) {
+
+    const [penState, setPenState] = useState<PenState>(PenState.Inactive);
+
+    const createTileHadleOnMouseDown = (position : Position) => {
+        return (event : Event) => {
+            if (event.target !== event.currentTarget) return;
+            if (Position.same(position, props.flagPosition) || Position.same(position, props.knightPosition)) return;
+
+            if (penState === PenState.Inactive) {
+                setPenState( props.getPassability(position) ? PenState.DrawImpassable : PenState.DrawPassable )
+                props.setPassability(position, !props.getPassability(position));
+            }
+        }
+    }
+
+    const createTileHadleOnMouseEnter = (position : Position) => {
+        return (event : Event) => {
+            if (event.target !== event.currentTarget) return;
+            if (Position.same(position, props.flagPosition) || Position.same(position, props.knightPosition)) return;
+
+            if (penState !== PenState.Inactive) {
+                props.setPassability(position, penState === PenState.DrawPassable ? true : false);
+            }
+        }
+    }
+
+    const createTileHadleOnMouseUp = () => {
+        return () => {
+            setPenState(PenState.Inactive);
+            }
+    }
+
+    const boardRef = useRef<HTMLDivElement>(null);
+
+    return <BaseBoard boardRef={boardRef}
+            height={props.height} width={props.width} getPassability={props.getPassability} knightPosition={props.knightPosition} flagPosition={props.flagPosition}
+            createTile={
+                (square, pos) => {
+                    return <SensibleTile
+                    key={`${pos.y},${pos.x}`}
+                    black={(pos.y + pos.x) % 2 === 1}
+                    onMouseDown={createTileHadleOnMouseDown(pos)}
+                    onMouseUp={createTileHadleOnMouseUp()}
+                    onMouseEnter={createTileHadleOnMouseEnter(pos)}
+                    passable={props.getPassability(pos)}>
+
+                    {/* Put knight and flag to their place */}
+
+                    { 
+                    Position.same(pos, props.knightPosition) ? 
+                        <ChessPiece child={KnightSVG} black={(pos.y + pos.x) % 2 === 0}/> : null
+                    }
+
+                    {
+                    Position.same(pos, props.flagPosition) ? 
+                    <ChessPiece child={FlagSVG} black={(pos.y + pos.x) % 2 === 0}/> : null
+                    }
+
+                    </SensibleTile>
+                }
             }/>
 }
