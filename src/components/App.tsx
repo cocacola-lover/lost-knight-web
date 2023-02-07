dd34b65e71b3023a67f74258139a01e94ab55a06
cocacola-lover <pegas620570@gmail.com>
2023-02-07 16:02:18 +0300

    To test

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,10 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Position from '../logic/position';
+import Mapping2D from '../logic/mapping2d';
 
-import { buildBoard } from '@cocacola-lover/knight_path_finder';
-import Board from './Board';
+import { Board, ChessPointer } from '@cocacola-lover/knight_path_finder';
+
+import {MovableBoard, DrawableBoard, DisplayBoard} from './Board';
 
 import './css/App.css';
 
@@ -10,23 +12,49 @@ function App() {
   const width = 8;
   const height = 8;
 
-  const board = buildBoard(width, height);
+  const [board, setBoard] = useState(new Mapping2D<boolean>(height, width, true));
 
   const [knightPosition, setKnightPosition] = useState<Position>(new Position(0, 0));
-  const [flagPosition, setFlagPosition] = useState<Position>(new Position(0, 1));
+  const [flagPosition, setFlagPosition] = useState<Position>(new Position(3, 5));
+
+
+  const getPassability = (pos : Position) => {
+    return board.at(pos);
+  }
+
+  const setPassability = (pos : Position, value : boolean) => {
+    setBoard((previousState) => {
+      const newState = previousState.copy();
+      newState.setAt(pos, value);
+      return newState;
+    });
+  }
+
+  // return (
+  //   <div className='App'>
+  //     <MovableBoard 
+  //     width={width} 
+  //     height={height} 
+  //     squares={board.squares}
+  //     knightPosition={knightPosition}
+  //     setKnightPosition={setKnightPosition}
+  //     flagPosition={flagPosition}
+  //     setFlagPosition={setFlagPosition}/>
+  //   </div>
+  // );
 
   return (
     <div className='App'>
-      <Board 
+      <DisplayBoard 
       width={width} 
       height={height} 
-      squares={board.squares}
       knightPosition={knightPosition}
-      setKnightPosition={setKnightPosition}
       flagPosition={flagPosition}
-      setFlagPosition={setFlagPosition}/>
+      getPassability={getPassability}
+      />
     </div>
-  );
+  )
 }
+
 
 export default App;
