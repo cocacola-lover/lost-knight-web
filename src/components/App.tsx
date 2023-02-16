a1162d2263e45249fd19066dfd7a2569d8494723
cocacola-lover <pegas620570@gmail.com>
2023-02-16 20:40:02 +0300

    Put everything together

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,33 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useCallback} from 'react';
 import Position from '../logic/position';
 import Mapping2D from '../logic/mapping2d';
 
-import { Board, ChessPointer } from '@cocacola-lover/knight_path_finder';
-
+import SettingManager from './SettingsManager';
+import { AppInterfaces } from '../logic/interfaces';
 import {MovableBoard, DrawableBoard, DisplayBoard} from './Board';
+
+import BoardState = AppInterfaces.BoardState;
 
 import './css/App.css';
 
 function App() {
-  const width = 8;
-  const height = 8;
 
+  const [width, setWidth] = useState(10);
+  const [height, setHeight] = useState(10);
+
+  const [boardState, setBoardState] = useState(BoardState.Movable);
   const [board, setBoard] = useState(new Mapping2D<boolean>(height, width, true));
 
   const [knightPosition, setKnightPosition] = useState<Position>(new Position(0, 0));
-  const [flagPosition, setFlagPosition] = useState<Position>(new Position(3, 5));
+  const [flagPosition, setFlagPosition] = useState<Position>(new Position(1, 1));
 
 
-  const getPassability = (pos : Position) => {
-    return board.at(pos);
-  }
+  const getPassability = useCallback((pos : Position) => {
+    try {
+      return board.at(pos);
+    } catch (e) {}
+    return true;
+  }, [board]);
 
   const setPassability = (pos : Position, value : boolean) => {
     setBoard((previousState) => {
@@ -30,28 +37,85 @@ function App() {
     });
   }
 
-  // return (
-  //   <div className='App'>
-  //     <MovableBoard 
-  //     width={width} 
-  //     height={height} 
-  //     squares={board.squares}
-  //     knightPosition={knightPosition}
-  //     setKnightPosition={setKnightPosition}
-  //     flagPosition={flagPosition}
-  //     setFlagPosition={setFlagPosition}/>
-  //   </div>
-  // );
+  // Work with scaling
+  useEffect(() => {
+    setBoard((prevBoard) => {
+      return prevBoard.scaleTo(height, width, true);
+    });
+
+    setKnightPosition((prevPosition) => new Position(
+          prevPosition.x < width ? prevPosition.x : width - 1,
+          prevPosition.y < height ? prevPosition.y : height - 1,
+    ));
+
+    setFlagPosition((prevPosition) => new Position(
+      prevPosition.x < width ? prevPosition.x : width - 1,
+      prevPosition.y < height ? prevPosition.y : height - 1,
+    ));
+
+  }, [width, height]);
+
+  // Collision prevention
+  useEffect(() => {
+    if (Position.same(knightPosition, flagPosition)) {
+      setKnightPosition(new Position(0, 0));
+      setFlagPosition(new Position(1, 1));
+    }
+
+    if (!getPassability(knightPosition)) setPassability(knightPosition, true);
+    if (!getPassability(flagPosition)) setPassability(flagPosition, true);
+  }, [knightPosition, flagPosition, getPassability])
 
   return (
     <div className='App'>
-      <DisplayBoard 
-      width={width} 
-      height={height} 
-      knightPosition={knightPosition}
-      flagPosition={flagPosition}
-      getPassability={getPassability}
-      />
+      <div className='SettingsManagerWrapper'>
+
+      <SettingManager
+      width={{get : width, set : setWidth}}
+      height={{get : height, set : setHeight}}
+      iterate={{get : boardState === BoardState.Display, toggle : () => setBoardState((prevState) => {
+        return (prevState === BoardState.Display) ? BoardState.Movable : BoardState.Display;
+      })}}
+      draw={{get : boardState === BoardState.Drawable, toggle : () => setBoardState((prevState) => {
+        return (prevState === BoardState.Drawable) ? BoardState.Movable : BoardState.Drawable;
+      })}}
+      ></SettingManager>
+
+      </div>
+
+      {
+        (() => {
+          switch (boardState) {
+            case BoardState.Movable :
+              return <MovableBoard 
+              width={width} 
+              height={height} 
+              knightPosition={knightPosition}
+              flagPosition={flagPosition}
+              setFlagPosition={setFlagPosition}
+              setKnightPosition={setKnightPosition}
+              getPassability={getPassability}
+              />
+            case BoardState.Drawable :
+              return <DrawableBoard
+              width={width} 
+              height={height} 
+              knightPosition={knightPosition}
+              flagPosition={flagPosition}
+              getPassability={getPassability}
+              setPassability={setPassability}
+              />
+            case BoardState.Display : 
+              return <DisplayBoard
+              width={width} 
+              height={height} 
+              knightPosition={knightPosition}
+              flagPosition={flagPosition}
+              getPassability={getPassability}
+              passabilityMap={board}/>
+          }
+        })()
+      }
     </div>
   )
 }
