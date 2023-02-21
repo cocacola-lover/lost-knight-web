f85f7dda77ff7ef29e7d35783c29490f82138550
cocacola-lover <pegas620570@gmail.com>
2023-02-22 00:29:09 +0300

    Added convenience method

--- src/logic/mapping2d.ts
+++ src/logic/mapping2d.ts
@@ -22,31 +22,31 @@ export default class Mapping2D<Type> {
         return this.arr[position.x][position.y];
     }
 
-    safeAt(position : Position, defaultVal : Type) {
-        if (this.arr[position.x] !== undefined && this.arr[position.x][position.y] !== undefined)
-            return this.arr[position.x][position.y]
-        else return defaultVal;
-    }
-
     setAt (position : Position, value : Type) {
         this.arr[position.x][position.y] = value;
-    }
-
-    safeSetAt(position : Position, value : Type) {
-        if (this.arr[position.x] !== undefined && this.arr[position.x][position.y] !== undefined)
-            this.arr[position.x][position.y] = value;
     }
 
     map (func : (value : Type, index1 : number, index : number) => Type) {
         const ans = this.copy();
 
-        for (let i = 0; i++; i < ans.arr.length) {
-            for (let j = 0; j++; j < ans.arr[0].length)
+        for (let i = 0; i < ans.arr.length; i++) {
+            for (let j = 0; j < ans.arr[0].length; j++)
                 ans.arr[i][j] = func(ans.arr[i][j], i, j);
         }
 
         return ans;
     }
+
+    static converterMap<TypeFrom, TypeTo> (
+            from : Mapping2D<TypeFrom>, using : (value : TypeFrom) => TypeTo) {
+                const ans = new Mapping2D<TypeTo>(from.height, from.width, using(from.at(new Position(0, 0))));
+
+                from.forEach((value, index1, index2) => {
+                    ans.setAt(new Position(index1, index2), using(value));
+                })
+
+                return ans;
+            }
 
     forEach (func : (value : Type, index1 : number, index : number) => any) {
         for (let i = 0; i < this.arr.length; i++) {
