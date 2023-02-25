1ae4f10a24bb9bc93f076b98973266ab3373b670
cocacola-lover <pegas620570@gmail.com>
2023-02-25 22:26:46 +0300

    Created function to build Weight map

---
+++ src/logic/weightMap.ts
@@ -0,0 +1,29 @@
+import Mapping2D from "./mapping2d";
+import { Settings } from "./interfaces";
+
+export function createWeightMap (height : number, width : number, settings : Settings.WeightSettings) {
+    let ans = new Mapping2D<number>(height, width, 0);
+
+    if (settings.avoidBlack) {
+        ans = ans.map((value, index1, index2) => {
+            return (index1 + index2) % 2 === 0 ? value : value + 1;
+        });
+    }
+    if (settings.avoidWhite) {
+        ans = ans.map((value, index1, index2) => {
+            return (index1 + index2) % 2 === 1 ? value : value + 1;
+        });
+    }
+    if (settings.avoidCorners) {
+        ans = ans.map((value, index1, index2) =>{
+            return value + Math.sqrt((index1 - (height - 1) / 2) * (index1 - (height - 1) / 2) + (index2 - (width - 1) / 2) * (index2 - (width - 1) / 2));
+        })
+    }
+    if (settings.avoidCenter) {
+        ans = ans.map((value, index1, index2) =>{
+            return value - Math.sqrt((index1 - (height - 1) / 2) * (index1 - (height - 1) / 2) + (index2 - (width - 1) / 2) * (index2 - (width - 1) / 2));
+        })
+    }
+
+    return ans;
+}
